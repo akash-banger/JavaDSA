3b4d1c56da5f43180f82441c563d53f8678d993e
akash-banger <akashbanger2@gmail.com>
2022-11-29 05:04:52 +0530

    yes

---
+++ JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics { // file name and the class name should be same .!!
+    public static void main(String arg[]){
+        System.out.print("Hello World!!\n");
+        System.out.println("Hello World??"); /// println will give a line space after printing your value.
+    }
+}
+// BoilerPlate code 
+
+
+// for compiling: javac filename.java
+// for running : java filename.java 
+
